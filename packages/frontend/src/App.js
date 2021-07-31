27c912cdca726d6f201f1a43aa4386bd53d2fde0
Florin C <cozloschi.florin@gmail.com>
2021-07-31 16:17:16 +0300

    chore: add boilerplate for backend and frontend

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         My first app
+      </header>
+    </div>
+  );
+}
+
+export default App;
